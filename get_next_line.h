3cf391d0c2de32ccc2f26298e0e7d96d29ba8c69
Julien DUMAY <judumay@student.42.fr>
2018-11-26 17:23:03 +0100

    gnl

---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: judumay <judumay@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/13 13:52:35 by judumay           #+#    #+#             */
+/*   Updated: 2018/11/24 19:14:27 by judumay          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft/libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+# define BUFF_SIZE 1
+
+int get_next_line(const int fd, char **line);
+
+#endif
---
+++ main.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: judumay <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/26 16:50:23 by judumay           #+#    #+#             */
+/*   Updated: 2018/11/26 16:53:05 by judumay          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
+
+void	ft_exit(const char *str)
+{
+	if (str)
+		ft_putendl(str);
+	exit(1);
+}
+
+int		main(int argc, char **argv)
+{
+	char	*line;
+	int	fd;
+	int	code;
+	int	len;
+	int	i;
+
+	i = 1;
+	printf("BUFF_SIZE = %d\n", BUFF_SIZE);
+	printf("Premier FD\n\n");
+	while (i < argc)
+	{
+		if ((fd = open(argv[i], O_RDONLY)) == -1)
+			ft_exit("Error: Can't open file");
+		while ((code = get_next_line(fd, &line)) > 0)
+		{
+			len = ft_strlen(line);
+			printf("%d - %d - |%s|\n", code, len, line);
+			free(line);
+		}
+		i++;
+		if (i < argc)
+			printf("\n%d eme FD\n\n", i);
+	}
+	printf("LAST %d - |%s|\n", code, line);
+	return (0);
+}
